31b573f0a1a6644054bf995ae478f2e7c4757640
Camigamba1 <88547761+Camigamba1@users.noreply.github.com>
2021-08-29 20:48:08 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,45 @@
 ﻿using System;
 
-namespace Ejercicio3
+namespace Ejercicio2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 5; i++)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Ingrese un número");
-                int num = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-                Console.Write(i + ". "+ num + " ");
-                for (int j = 0; j < num; j++)
+            Console.WriteLine("Ingrese el número de latas");
+            int num = int.Parse(Console.ReadLine());
+            int[] arreglo = new int[num];
+            llenarMatriz(arreglo);
+            Boolean esApilable = comprobarNumero(arreglo, num);
+            if(esApilable){
+                Console.WriteLine("El número es apilable");
+                for (int i = 1; i <= num; i++)
                 {
                     Console.Write("* ");
+                    if(comprobarNumero(arreglo, i)){
+                        Console.WriteLine();
+                    }        
                 }
-                Console.WriteLine();
+            }else{
+                Console.WriteLine("El número no es apliable");
             }
+        }
+
+        static void llenarMatriz(int[] array){
+           for (int i = 0; i < array.Length; i++)
+           {
+               array[i] = (i+1)*(i+2)/2;
+           }
+        }
+        static Boolean comprobarNumero(int[] array, int n){
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (n == array[i])
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
